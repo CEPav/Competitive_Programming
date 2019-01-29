e78ce397833f206c4a7e71a820e11980fa4853ca
Clara Eleonore Pavillet <clara.pavillet@msdtc.ox.ac.uk>
2019-01-29 17:40:35 -0500

    Create FirstReverse.py

---
+++ CoderByte/Challenges/FirstReverse.py
@@ -0,0 +1,9 @@
+# Question
+# Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order
+# Ex. if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH
+
+# Solution
+# Time Complexity = O(n) where n = length of string
+
+def FirstReverse(str):
+    return str[::-1]
