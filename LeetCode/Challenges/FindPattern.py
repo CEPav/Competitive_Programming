ac13fc4f49d0e8659c16ed146725dc268b4af3d2
Clara Eléonore Pavillet <clara.pavillet@msdtc.ox.ac.uk>
2019-01-30 16:01:06 -0500

    Create FindPattern.py

---
+++ LeetCode/Challenges/FindPattern.py
@@ -0,0 +1,26 @@
+# Quick Solution
+# 36 ms
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
+       
+# 'Manual' Solution -> Faster!
+# 32 ms, faster than 100.00% of Python3 online submissions
+
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle in haystack:
+            return haystack.index(needle)
+        else:
+            return -1
+        
