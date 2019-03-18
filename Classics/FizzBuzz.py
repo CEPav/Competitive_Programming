1bbef6529f5a8096a32e98c02cd7174373767fc9
Clara Eléonore Pavillet <clara.pavillet@msdtc.ox.ac.uk>
2019-03-18 00:26:59 +0000

    Create FizzBuzz.py

---
+++ Classics/FizzBuzz.py
@@ -0,0 +1,13 @@
+def FizzBuzz(n):
+    for integer in range(1,n+1):
+        if integer % 3 == 0 and integer % 5 != 0:
+            print('Fizz')
+        elif integer % 5 == 0 and integer % 3 != 0:
+            print('Buzz')
+        elif integer % 3 == 0 and integer % 5 == 0:
+                print('FizzBuzz')
+        else:
+            print(integer)
+
+n = input()
+FizzBuzz(int(n))
