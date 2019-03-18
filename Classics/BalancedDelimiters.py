0899193ac27a15b653406b978c290e96e5c39f58
Clara Eléonore Pavillet <clara.pavillet@msdtc.ox.ac.uk>
2019-03-18 15:56:08 +0000

    Create BalancedDelimiters.py

---
+++ Classics/BalancedDelimiters.py
@@ -0,0 +1,21 @@
+# Clara Eleonore Pavillet
+
+def BalanceCheck(delimiters):
+    characters = {'(':')', '[':']', '{':'}'}
+    stack = []
+    for char in delimiters:
+        if char in characters.keys():
+            stack.append(char)
+        elif char in characters.values():
+            if not stack: #If empty then we dont want to close so False
+                return False
+            elif characters[stack.pop()] != char:
+                return False
+        else:
+            return False #Any other character not valid
+    return not stack #True if empty - thus all balanced
+
+    
+delimiters = str(input())
+balance = BalanceCheck(delimiters)
+print(balance)
